f0e180de4654e17b50e7182f0452b773f8b37e7b
s_lucas <splucas@gmail.com>
2023-06-10 10:09:54 -0400

    Lane Marker refactor

--- Assets/Scripts/LaneLineMover.cs
+++ Assets/Scripts/LaneLineMover.cs
@@ -2,16 +2,28 @@ using UnityEngine;
 
 public class LaneLineMover : MonoBehaviour
 {
-
+    // Normal Line Movement Speed
     public float Speed = 2;
+    // Movement Speed when player presses "up"
     public float HighSpeed = 6;
 
+    // Scene camera; if left null, uses main
+    public Camera ViewCamera = null;
+
+    // Spawned gameobject that represents the lane markers
+    public GameObject LinesPrefab = null;
+    // Distance between the marker objects
+    public float LineSpacing = 3.5f;
+
     float currentSpeed;
+    float yReset;
 
 
     private void Awake()
     {
         currentSpeed = Speed;
+        if (ViewCamera == null)
+            ViewCamera = Camera.main;
     }
     private void OnEnable()
     {
@@ -21,7 +33,35 @@ public class LaneLineMover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        DrawLaneMarkers();
+    }
+
+
+    /// <summary>
+    /// Draws lane markers based on the number of markers needed to fill the camera viewport
+    /// space.
+    /// </summary>
+    void DrawLaneMarkers()
+    {
+        Vector3 topCorner = ViewCamera.ViewportToWorldPoint(new Vector3(0, 1, ViewCamera.nearClipPlane));
+
+        int numMarkers = Mathf.CeilToInt(topCorner.y / LineSpacing);
+        for (int cnt = 0; cnt <= numMarkers; cnt++)
+        {
+            yReset = cnt * LineSpacing;
+
+            Vector3 newPos = new Vector3(0, yReset, 0);
+            GameObject newLaneMarker = Instantiate(LinesPrefab, newPos, Quaternion.identity);
+            newLaneMarker.transform.SetParent(transform, true);
+            // Draw the lower markers
+            if (yReset > 0.0f)
+            {
+                newPos.y = -yReset;
+                newLaneMarker = Instantiate(LinesPrefab, newPos, Quaternion.identity);
+                newLaneMarker.transform.SetParent(transform, true);
+            }
+        }
+
     }
 
     public void GoFast(bool tf)
@@ -42,8 +82,9 @@ public class LaneLineMover : MonoBehaviour
             Vector3 pos = kid.position;
             pos.y -= Time.deltaTime * currentSpeed;
 
-            if (pos.y < -14.2)
-                pos.y = 14.0f;
+            if (pos.y < -yReset)
+                pos.y = yReset;
+
 
             kid.position = pos;
 
